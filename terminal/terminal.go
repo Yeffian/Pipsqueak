daf76418c977099c5c9f76cd002161f4132f062b
YeffyCodeGit <aditchakraborty2009@gmail.com>
2022-01-25 17:41:40 +0600

    removed main file

--- terminal/terminal.go
+++ terminal/terminal.go
@@ -3,5 +3,7 @@ package terminal
 import "fmt"
 
 func Clear() {
-	fmt.Println("\x1b[2J")
+	// this doesnt actually properly clear the terminal, more just scrolls down so you cant see anything anymore, you can break the illusion by
+	// just scrolling up, but its fine for now
+	fmt.Println("\033[H\033[2J")
 }
